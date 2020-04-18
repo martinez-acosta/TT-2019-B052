1f7ce5ebca4ffc264c93affee2f14e1e44f3b9b0
lalo <fx2013630461@gmail.com>
2020-04-18 05:34:19 -0500

    Agregada pagina de signup con validaciones

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,81 +1,84 @@
 <template>
-  <v-container outlined tile flat width="400" class="mx-auto mt-5">
-    <h1>Signup</h1>
-    <v-form>
-      <v-text-field
-        v-model="name"
-        :error-messages="nameErrors"
-        :counter="40"
-        label="Name"
-        required
-        prepend-icon="mdi-format-text"
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-      ></v-text-field>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" center>
+          <h1>Signup</h1>
+          <v-form>
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              :counter="40"
+              label="Name"
+              prepend-icon="mdi-format-text"
+              required
+              @input="$v.name.$touch()"
+              @blur="$v.name.$touch()"
+            ></v-text-field>
 
-      <v-text-field
-        v-model="username"
-        :error-messages="usernameErrors"
-        :counter="10"
-        label="Username"
-        required
-        prepend-icon="mdi-format-text"
-        @input="$v.username.$touch()"
-        @blur="$v.username.$touch()"
-      ></v-text-field>
+            <v-text-field
+              v-model="username"
+              :error-messages="usernameErrors"
+              :counter="10"
+              label="Username"
+              prepend-icon="mdi-format-text"
+              required
+              @input="$v.username.$touch()"
+              @blur="$v.username.$touch()"
+            ></v-text-field>
 
-      <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="show ? 'text' : 'password'"
-        label="Password"
-        placeholder="Enter password"
-        required
-        prepend-icon="mdi-form-textbox-password"
-        @click:append="show = !show"
-      />
-      <v-text-field
-        v-model="passwordConfirmation"
-        :rules="passwordRules"
-        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="show ? 'text' : 'password'"
-        label="Password"
-        placeholder="Confirm Password"
-        required
-        tile
-        prepend-icon="mdi-form-textbox-password"
-        @click:append="show = !show"
-      />
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="Password"
+              prepend-icon="mdi-form-textbox-password"
+              required
+              @click:append="show = !show"
+            />
+            <v-text-field
+              v-model="passwordConfirmation"
+              :rules="passwordRules"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="Confirm password"
+              required
+              prepend-icon="mdi-form-textbox-password"
+              @click:append="show = !show"
+            />
 
-      <v-file-input label="Attach profile picture"></v-file-input>
+            <v-file-input label="Attach profile picture"></v-file-input>
 
-      <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        type="email"
-        required
-        prepend-icon="mdi-email"
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
-      ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="E-mail"
+              type="email"
+              required
+              prepend-icon="mdi-email"
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+            ></v-text-field>
 
-      <v-checkbox
-        v-model="checkbox"
-        :error-messages="checkboxErrors"
-        label="Do you agree?"
-        required
-        @change="$v.checkbox.$touch()"
-        @blur="$v.checkbox.$touch()"
-      ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox"
+              :error-messages="checkboxErrors"
+              label="Do you agree?"
+              required
+              @change="$v.checkbox.$touch()"
+              @blur="$v.checkbox.$touch()"
+            ></v-checkbox>
 
-      <v-btn class="mr-4" color="primary" dark rounded @click="submit"
-        >submit</v-btn
-      >
-      <v-btn color="primary" rounded @click="clear">clear</v-btn>
-    </v-form>
-  </v-container>
+            <v-btn class="mr-4" color="primary" dark rounded @click="submit"
+              >submit</v-btn
+            >
+            <v-btn color="primary" rounded @click="clear">clear</v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 <script>
 import { validationMixin } from 'vuelidate'
