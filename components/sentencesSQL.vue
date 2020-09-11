b754bc1cc7b7ef70cb3e97942854ad04e513f7c7
omar <omaraparicio07@gmail.com>
2020-09-11 18:38:47 -0500

    coding #3 Agregando componente para las sentencias SQL

---
+++ components/sentencesSQL.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="row col">
+      <v-toolbar dense>
+        <v-btn icon>
+          <v-icon>mdi-content-save</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-content-paste</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-help</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
+    <div class="row col">
+      <code class="container border">
+        {{ sentences }}
+      </code>
+    </div>
+  </div>
+</template>
+
+<script type="module">
+export default {
+  data() {
+    return {
+      sentences: `
+      CREATE TABLE [IF NOT EXISTS] table_name(
+        column_1_definition,
+        column_2_definition,
+        ...,
+        table_constraints
+        ) ENGINE=storage_engine;
+      `
+    }
+  }
+}
+</script>
---
+++ pages/workspace/sentencesSQL.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-content>
+    <sentencesSQL />
+  </v-content>
+</template>
+
+<script>
+import sentencesSQL from '@/components/sentencesSQL'
+
+export default {
+  layout: 'workspace',
+  components: {
+    sentencesSQL
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
