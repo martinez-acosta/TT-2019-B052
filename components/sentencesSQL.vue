77d9d3946719db6a6dc2538858b7168c83f592e2
omar <omaraparicio07@gmail.com>
2020-09-11 20:03:29 -0500

    coding #3 Prueba de concepto del componente ssh-pre con un ejemplo de script SQL

--- components/sentencesSQL.vue
+++ components/sentencesSQL.vue
@@ -3,7 +3,7 @@
     <div class="row col">
       <v-toolbar dense>
         <v-btn icon>
-          <v-icon>mdi-content-save</v-icon>
+          <v-icon>mdi-download</v-icon>
         </v-btn>
         <v-btn icon>
           <v-icon>mdi-content-paste</v-icon>
@@ -14,26 +14,64 @@
         </v-btn>
       </v-toolbar>
     </div>
-    <div class="row col">
-      <code class="container border">
-        {{ sentences }}
-      </code>
+    <div class="comtaier overflow-auto">
+      <ssh-pre language="sql" label="SQL">{{ sentences }}</ssh-pre>
     </div>
   </div>
 </template>
 
 <script type="module">
+import SshPre from 'simple-syntax-highlighter'
+import 'simple-syntax-highlighter/dist/sshpre.css'
+
 export default {
+  components: { SshPre },
   data() {
     return {
-      sentences: `
-      CREATE TABLE [IF NOT EXISTS] table_name(
-        column_1_definition,
-        column_2_definition,
-        ...,
-        table_constraints
-        ) ENGINE=storage_engine;
-      `
+      sentences: `/*
+*********************************************************************
+Name: MySQL Sample Database classicmodels
+Version 3.1
++ changed data type from DOUBLE to DECIMAL for amount columns
+Version 3.0
++ changed DATETIME to DATE for some colunmns
+Version 2.0
++ changed table type from MyISAM to InnoDB
++ added foreign keys for all tables 
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+CREATE DATABASE IF NOT EXISTS 'classicmodels';
+
+USE 'classicmodels';
+
+/*Table structure for table 'customers' */
+
+DROP TABLE IF EXISTS 'customers';
+
+CREATE TABLE 'customers' (
+  'customerNumber' int(11) NOT NULL,
+  'customerName' varchar(50) NOT NULL,
+  'contactLastName' varchar(50) NOT NULL,
+  'contactFirstName' varchar(50) NOT NULL,
+  'phone' varchar(50) NOT NULL,
+  'addressLine1' varchar(50) NOT NULL,
+  'addressLine2' varchar(50) DEFAULT NULL,
+  'city' varchar(50) NOT NULL,
+  'state' varchar(50) DEFAULT NULL,
+  'postalCode' varchar(15) DEFAULT NULL,
+  'country' varchar(50) NOT NULL,
+  'salesRepEmployeeNumber' int(11) DEFAULT NULL,
+  'creditLimit' decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY ('customerNumber'),
+  KEY 'salesRepEmployeeNumber' ('salesRepEmployeeNumber'),
+  CONSTRAINT 'customers_ibfk_1' FOREIGN KEY ('salesRepEmployeeNumber') REFERENCES 'employees' ('employeeNumber')
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+`
     }
   }
 }
