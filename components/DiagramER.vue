75e83dac8cfc526e7cddc2a5fa0e0bc90f3f1607
omar <omar@makingdevs.com>
2020-09-15 13:04:18 -0500

    coding #3 Agregando menú de botones superiores al diagramador

--- components/DiagramER.vue
+++ components/DiagramER.vue
@@ -1,5 +1,53 @@
 <template>
   <v-container fill-height fluid ma-0 pa-0>
+    <v-row>
+      <v-toolbar dense>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click="saveModel()">
+              <v-icon>mdi-content-save</v-icon>
+            </v-btn>
+          </template>
+          <span>Guardar</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-folder-open</v-icon>
+            </v-btn>
+          </template>
+          <span>Abrir...</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-file</v-icon>
+            </v-btn>
+          </template>
+          <span>Nuevo</span>
+        </v-tooltip>
+        <v-divider class="m-0 p-0" vertical></v-divider>
+        <v-btn text large color="primary">Validar diagrama</v-btn>
+        <v-divider class="mx-4" vertical></v-divider>
+        <v-spacer></v-spacer>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+              @click.stop="helpDialog = true"
+            >
+              <v-icon>mdi-help</v-icon>
+            </v-btn>
+          </template>
+          <span>Ayuda</span>
+        </v-tooltip>
+      </v-toolbar>
+    </v-row>
     <v-row
       no-gutters
       align="center"
@@ -603,7 +651,7 @@ export default {
       this.saveDiagramProperties()
       this.savedModel = this.myDiagram.model.toJson()
       this.myDiagram.isModified = false
-      //  console.log('savedModel: ' + this.savedModel)
+      // console.log('savedModel: ' + this.savedModel)
       this.$store.dispatch('diagramER/save', {
         savedModel: this.savedModel
       })
