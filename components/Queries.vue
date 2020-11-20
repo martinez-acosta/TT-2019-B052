f5f76633b6fb47309e7fa26cbe81df357a08fba4
lalo <fx2013630461@gmail.com>
2020-11-19 23:12:34 -0600

    Avance consultas de acceso

--- components/Queries.vue
+++ components/Queries.vue
@@ -1,34 +1,38 @@
 <template>
-  <v-card>
-    <v-tabs v-model="tab" background-color="blue darken-3" dark show-arrows>
-      <v-tab v-for="n in length" :key="n"> Q{{ n }} </v-tab>
-      <v-btn icon>
-        <v-icon @click="length++">mdi-plus</v-icon>
-      </v-btn>
-    </v-tabs>
-    <!--<v-card-text class=">
-      <v-btn :disabled="!length" text @click="length--">
-        Remove Query
-      </v-btn>
-      <v-divider class="mx-4" vertical></v-divider>
-      <v-btn text @click="length++">
-        Add Query
-      </v-btn>
-    </v-card-text>-->
-  </v-card>
+  <v-container>
+    <v-row>
+      <v-col cols="10">
+        <v-tabs v-model="tab" background-color="white" show-arrows flat>
+          <v-tab v-for="n in length" :key="n"> Q{{ n }} </v-tab>
+        </v-tabs>
+      </v-col>
+      <v-col cols="2">
+        <v-tabs>
+          <v-btn v-show="length - 1" icon>
+            <v-icon @click="length--">mdi-minus</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon @click="length++">mdi-plus</v-icon>
+          </v-btn>
+        </v-tabs>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    length: 1,
-    tab: null
-  }),
-
+  data() {
+    return {
+      length: 1,
+      tab: null
+    }
+  },
   watch: {
     length(val) {
       this.tab = val - 1
     }
-  }
+  },
+  methods: {}
 }
 </script>
