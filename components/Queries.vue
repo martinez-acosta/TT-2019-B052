7a36f572e97599fd302ca62e4161bd3bde1d0e61
lalo <fx2013630461@gmail.com>
2020-11-25 21:31:35 -0600

    avance

--- components/Queries.vue
+++ components/Queries.vue
@@ -13,45 +13,7 @@
         </v-tabs>
         <v-tabs-items v-model="tab">
           <v-tab-item v-for="n in length" :key="n">
-            <!------Consultas---------->
-            <v-container>
-              <v-row>
-                <v-col cols="6">
-                  <v-card>
-                    <v-card-title>
-                      Given
-                    </v-card-title>
-                    <v-data-table
-                      :headers="givenHeaders"
-                      hide-default-footer
-                      :items="givenEntries[n]"
-                    ></v-data-table>
-                  </v-card>
-                </v-col>
-                <v-col cols="6">
-                  <v-card>
-                    <v-card-title>
-                      Find
-                    </v-card-title>
-                    <v-data-table
-                      :headers="findHeaders"
-                      hide-default-footer
-                      :items="findEntries[n]"
-                    ></v-data-table>
-                  </v-card>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <textarea
-                    style="width:100%;height:300px"
-                    :value="diagramaObtenido"
-                  >
-                  </textarea>
-                </v-col>
-              </v-row>
-            </v-container>
-            <!------Fin consultas---------->
+            <Query />
           </v-tab-item>
         </v-tabs-items>
       </v-col>
@@ -73,8 +35,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
-
+import Query from '@/components/Query.vue'
 export default {
+  components: {
+    Query
+  },
   data() {
     return {
       givenHeaders: [
@@ -126,55 +91,15 @@ export default {
     if (this.queries) {
       for (let i = 1; i <= this.queries; i++) {}
     }
-    // listeners
-    this.$nuxt.$on('emitGivenValue', () => {
-      const nodo = this.nodoObtenido
-      const nodoConectado = this.nodoConectado
-
-      // this.givenEntries.push({
-      //  name: nodoConectado.text + '.' + nodo.text,
-      //  type: 'value'
-      // })
-      addEntry(nodo, nodoConectado)
-    })
-    // this.$nuxt.$on('emitGivenRange', () => {
-    //   const nodo = this.nodoObtenido
-    //   const nodoConectado = this.nodoConectado
-
-    //   this.givenEntries.push({
-    //     name: nodoConectado.text + '.' + nodo.text,
-    //     type: 'range'
-    //   })
-    // })
-    // this.$nuxt.$on('emitGivenSet', () => {
-    //   const nodo = this.nodoObtenido
-    //   const nodoConectado = this.nodoConectado
-
-    //   this.givenEntries.push({
-    //     name: nodoConectado.text + '.' + nodo.text,
-    //     type: 'set'
-    //   })
-    // })
-    // this.$nuxt.$on('emitFindValue', () => {
-    //   const nodo = this.nodoObtenido
-    //   const nodoConectado = this.nodoConectado
-
-    //   this.findEntries.push({
-    //     name: nodoConectado.text + '.' + nodo.text,
-    //     type: 'value'
-    //   })
-    // })
   },
-  beforeDestroy() {
-    this.$nuxt.$off('emitGivenValue')
-    this.$nuxt.$off('emitGivenRange')
-    this.$nuxt.$off('emitGivenSet')
-    this.$nuxt.$off('emitFindValue')
-  },
+  beforeDestroy() {},
 
   methods: {
     setLength(k) {
       this.$store.dispatch('vuexQueries/setLength', k)
+    },
+    onTabChange() {
+      console.log('Tab change')
     }
   }
 }
