63c34200128893f780180b60d88e2193dcc58303
Diyis <fx2013630461@gmail.com>
2020-11-25 23:03:16 -0600

    obtenido tab activa

--- components/Queries.vue
+++ components/Queries.vue
@@ -2,18 +2,50 @@
   <v-container>
     <v-row>
       <v-col cols="10">
-        <v-tabs
-          v-model="tab"
-          background-color="white"
-          show-arrows
-          flat
-          @change="onTabChange"
-        >
+        <v-tabs v-model="currentTab" background-color="white" show-arrows flat>
           <v-tab v-for="n in length" :key="n"> Q{{ n }} </v-tab>
         </v-tabs>
-        <v-tabs-items v-model="tab">
+        <v-tabs-items v-model="currentTab">
           <v-tab-item v-for="n in length" :key="n">
-            <Query />
+            <!------Consultas---------->
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-card>
+                    <v-card-title>
+                      Given
+                    </v-card-title>
+                    <v-data-table
+                      :headers="givenHeaders"
+                      hide-default-footer
+                      :items="givenEntries[n]"
+                    ></v-data-table>
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <v-card>
+                    <v-card-title>
+                      Find
+                    </v-card-title>
+                    <v-data-table
+                      :headers="findHeaders"
+                      hide-default-footer
+                      :items="findEntries[n]"
+                    ></v-data-table>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <textarea
+                    style="width:100%;height:300px"
+                    :value="diagramaObtenido"
+                  >
+                  </textarea>
+                </v-col>
+              </v-row>
+            </v-container>
+            <!------Fin consultas---------->
           </v-tab-item>
         </v-tabs-items>
       </v-col>
@@ -35,11 +67,8 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Query from '@/components/Query.vue'
+
 export default {
-  components: {
-    Query
-  },
   data() {
     return {
       givenHeaders: [
@@ -66,8 +95,7 @@ export default {
       ],
       // givenEntries: [],
       // findEntries: [],
-      currentKey: 0,
-      tab: 0
+      currentTab: null
     }
   },
   computed: {
@@ -91,15 +119,54 @@ export default {
     if (this.queries) {
       for (let i = 1; i <= this.queries; i++) {}
     }
+    // listeners
+    this.$nuxt.$on('emitGivenValue', () => {
+      const nodo = this.nodoObtenido
+      const nodoConectado = this.nodoConectado
+
+      this.givenEntries.push({
+        name: nodoConectado.text + '.' + nodo.text,
+        type: 'value'
+      })
+    })
+    // this.$nuxt.$on('emitGivenRange', () => {
+    //   const nodo = this.nodoObtenido
+    //   const nodoConectado = this.nodoConectado
+
+    //   this.givenEntries.push({
+    //     name: nodoConectado.text + '.' + nodo.text,
+    //     type: 'range'
+    //   })
+    // })
+    // this.$nuxt.$on('emitGivenSet', () => {
+    //   const nodo = this.nodoObtenido
+    //   const nodoConectado = this.nodoConectado
+
+    //   this.givenEntries.push({
+    //     name: nodoConectado.text + '.' + nodo.text,
+    //     type: 'set'
+    //   })
+    // })
+    // this.$nuxt.$on('emitFindValue', () => {
+    //   const nodo = this.nodoObtenido
+    //   const nodoConectado = this.nodoConectado
+
+    //   this.findEntries.push({
+    //     name: nodoConectado.text + '.' + nodo.text,
+    //     type: 'value'
+    //   })
+    // })
   },
-  beforeDestroy() {},
+  beforeDestroy() {
+    this.$nuxt.$off('emitGivenValue')
+    this.$nuxt.$off('emitGivenRange')
+    this.$nuxt.$off('emitGivenSet')
+    this.$nuxt.$off('emitFindValue')
+  },
 
   methods: {
     setLength(k) {
       this.$store.dispatch('vuexQueries/setLength', k)
-    },
-    onTabChange() {
-      console.log('Tab change')
     }
   }
 }
